a3c4d8cb65f4ed5de97434fc158c10e83f96d05a
Om Bhesania <48543687+om-bhesania@users.noreply.github.com>
2023-08-18 14:20:08 +0530

    Update settings.py

--- ResumeBuilder/settings.py
+++ ResumeBuilder/settings.py
@@ -81,11 +81,26 @@ WSGI_APPLICATION = 'ResumeBuilder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uChVQyiBOeZOTSlLql1f',
+        'HOST': 'containers-us-west-196.railway.app',
+        'PORT': '6625',
+
     }
+
 }
 
 
